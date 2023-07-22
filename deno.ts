c30217b4fe7f2e5033fffb527839d000752c63ad
ydz <774404302@qq.com>
2023-07-22 10:39:02 +0800

    ✨

--- deno.ts
+++ deno.ts
@@ -1,4 +1,5 @@
 import { serve } from 'https://deno.land/std@0.181.0/http/server.ts'
+import { readAll } from 'https://deno.land/std@0.195.0/streams/read_all.ts'
 
 const OPENAI_API_HOST = 'api.openai.com'
 
@@ -9,6 +10,14 @@ serve(async (request) => {
     return fetch(new URL('./Readme.md', import.meta.url))
   }
 
+  readDataStream(request.body)
   url.host = OPENAI_API_HOST
   return await fetch(url, request)
 })
+
+// 读取request数据流
+const readDataStream = async (flow: any) => {
+  const stdinContent = await readAll(flow)
+
+  console.log(stdinContent.toString())
+}
