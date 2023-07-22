053c516b721016e79dac92d35719bc57001e609c
ydz <774404302@qq.com>
2023-07-22 10:11:19 +0800

    ✨

--- deno.ts
+++ deno.ts
@@ -1,14 +1,14 @@
-import { serve } from "https://deno.land/std@0.181.0/http/server.ts";
+import { serve } from 'https://deno.land/std@0.181.0/http/server.ts'
 
-const OPENAI_API_HOST = "api.openai.com";
+const OPENAI_API_HOST = 'api.openai.com'
 
 serve(async (request) => {
-  const url = new URL(request.url);
+  const url = new URL(request.url)
 
-  if (url.pathname === "/") {
-    return fetch(new URL("./Readme.md", import.meta.url));
+  if (url.pathname === '/') {
+    return fetch(new URL('./Readme.md', import.meta.url))
   }
-
-  url.host = OPENAI_API_HOST;
-  return await fetch(url, request);
-});
+  console.log(request.body)
+  url.host = OPENAI_API_HOST
+  return await fetch(url, request)
+})
